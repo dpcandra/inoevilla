e803c2089dd3ff4c735b9a4451aa9984a29fc530
dpcandra <dpcandra@gmail.com>
2023-01-05 21:17:22 +0800

    Add Navbar Logo, nav-item color and active color

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#">INOE VILLA</a>
+            <a class="navbar-brand" href="#"><img style="fill: #474747;" src="{{ asset("/images/Logovilla.svg") }}"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
