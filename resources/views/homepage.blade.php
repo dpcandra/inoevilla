f5ed80ba2590277a36e96af923af3fba06abc826
dpcandra <dpcandra@gmail.com>
2023-01-09 21:50:05 +0800

    Update Homepage, Navbar, Homepage background

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>its work!!!</h1>
+
+    <section class="jumbotron">
+        <div class="jumbo-title container h-100">
+            <div class="row h-100 align-items-end">
+                <div class="col-lg-5">
+                    <div class="bg-white d-flex flex-column px-3">
+                        <h1 class="mx-auto mt-4 text-uppercase">WELLCOME</h1>
+                        <h5 class="mx-auto my-3">Beauty nature meet cozy beach view</h5>
+                        <p class="text-center text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. A commodi necessitatibus sed dolore temporibus, sequi vitae vel alias. Totam, quae accusamus? Asperiores?</p>
+                    </div><!--bg-white-->
+                </div><!--col-lg--->
+            </div><!--row-->
+        </div><!--jumbo-title-->
+    </section>
+
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,6 +2,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="#"><img style="fill: #474747;" src="{{ asset("/images/Logovilla.svg") }}"></a>
+            <a href="" class="text-decoration-none text-muted">Contact Us : +(62) 822 8888 0962</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -20,7 +21,12 @@
                         <a class="nav-link text-uppercase" href="#">Photo Gallery</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-uppercase" href="#">Book</a>
+                        <a class="nav-link text-uppercase book-btn px-3 mx-3" href="#">Book</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase d-flex" href="#">
+                            <img class="search-icons" src="/icons/magnifying-glass-solid.svg" alt=""><span class="text-light">a</span>
+                        </a>
                     </li>
                 </ul>
             </div>
